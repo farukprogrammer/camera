05502876dc098e44a1c6b41e9d6197e0d29e2778
muhammad.rahman <muhammad.rahman@gopay.co.id>
2023-02-10 17:23:55 +0700

    add plugin for android

--- lib/camera.dart
+++ lib/camera.dart
@@ -17,3 +17,4 @@ export 'package:camera_platform_interface/camera_platform_interface.dart'
 export 'src/camera_controller.dart';
 export 'src/camera_image.dart';
 export 'src/camera_preview.dart';
+export 'src/android_camera.dart';
